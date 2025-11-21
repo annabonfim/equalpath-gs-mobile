c482429e16939b4138e1959c61740a43e00c0e8d
annabonfim <annabonfim@icloud.com>
2025-11-21 14:05:38 -0300

    feat: adicionar configuração de ESLint e Prettier
    - Configurar ESLint com regras para React Native
    - Configurar Prettier para formatação de código
    - Adicionar scripts npm para lint e format
    - Atualizar .gitignore para incluir caches do ESLint/Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['react', 'react-native', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/split-platform-components': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
